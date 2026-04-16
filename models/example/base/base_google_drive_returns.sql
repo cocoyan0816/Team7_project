aa33998793173c7c572802af8035680d52e01f8b
sylvaxia0211@gmail.com <sylvaxia0211@gmail.com>
2026-04-16 16:41:52 +0000

    drop the example folder and change the base under the models file directly.

--- models/example/base/base_google_drive_returns.sql
+++
@@ -1,9 +0,0 @@
-select
-    _file,
-    _line,
-    _modified as modified_ts,
-    _fivetran_synced as _fivetran_synced_ts,
-    is_refunded,
-    cast(order_id as string) as order_id,
-    returned_at
-from {{ source('google_drive', 'returns') }}
